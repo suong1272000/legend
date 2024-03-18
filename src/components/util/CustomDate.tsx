7750f07f3b28bc6b6f12ae53a7783c872a58da9b
kseonghee <kseonghee0127@gmail.com>
2024-03-18 15:41:22 +0900

    feat: 12시간제 변경

--- src/components/util/CustomDate.tsx
+++ src/components/util/CustomDate.tsx
@@ -1,34 +1,49 @@
 import { CSSProperties } from "react";
-import { fontSize, fontWeight, labelColor, lineHeight } from "../atoms/Labels/Common.styles";
-import ParagraphLabel, { ParagraphLabelProps } from "../atoms/Labels/ParagraphLabel";
+import {
+	fontSize,
+	fontWeight,
+	labelColor,
+	lineHeight,
+} from "../atoms/Labels/Common.styles";
+import ParagraphLabel, {
+	ParagraphLabelProps,
+} from "../atoms/Labels/ParagraphLabel";
 
 const CustomDate = () => {
-    const today = new Date();
-    const formattedDate = `${today.getFullYear()}/${today.getMonth() + 1}/${today.getDate()} ${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+	const today = new Date();
+	const minutes = today.getMinutes().toString().padStart(2, "0");
+	const seconds = today.getSeconds().toString().padStart(2, "0");
+	const hours = (today.getHours() % 12 || 12).toString().padStart(2, "0");
+	const formattedDate = `${today.getFullYear()}/${
+		today.getMonth() + 1
+	}/${today.getDate()} ${hours}:${minutes}:${seconds}`;
 
-    const TimePgLabelStyles:ParagraphLabelProps = {
-        common: {
-            color: labelColor['white'],
-            fontSize: fontSize['small'],
-            fontWeight: fontWeight['small'],
-            lineHeight: lineHeight['large'],
-        },
-        content: `${formattedDate} 기준`,
-        style: {
-            marginLeft: "15px"
-        }
-    }
-    const Styles:CSSProperties = {
-        alignItems: "center",
-        cursor: "pointer",
-        display: "flex"
-    }
+	const TimePgLabelStyles: ParagraphLabelProps = {
+		common: {
+			color: labelColor["white"],
+			fontSize: fontSize["small"],
+			fontWeight: fontWeight["small"],
+			lineHeight: lineHeight["large"],
+		},
+		content: `${formattedDate} 기준`,
+		style: {
+			marginLeft: "15px",
+		},
+	};
+	const Styles: CSSProperties = {
+		alignItems: "center",
+		cursor: "pointer",
+		display: "flex",
+	};
 
-    return (
-        <a style={Styles}>
-            <img src="/reload.c8f3f334ef34379ab3f101cc470d3cb1.svg" alt="reload" />
-            <ParagraphLabel {...TimePgLabelStyles} />
-        </a>
-    )
-}
-export default CustomDate
+	return (
+		<a style={Styles}>
+			<img
+				src="/reload.c8f3f334ef34379ab3f101cc470d3cb1.svg"
+				alt="reload"
+			/>
+			<ParagraphLabel {...TimePgLabelStyles} />
+		</a>
+	);
+};
+export default CustomDate;
