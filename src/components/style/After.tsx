a7ed399d894495cf56a03884d81983bdf213ae59
suong1272000 <suong1272000@gmail.com>
2024-06-12 18:44:07 +0900

    feat: 수정

--- src/components/style/After.tsx
+++ src/components/style/After.tsx
@@ -1,17 +1,17 @@
 import styled from '@emotion/styled';
 
 const After = styled.div`
-        background: url(/Pattern02.5888be2b14b5c0d295ea.svg) no-repeat;
-    bottom: 15%;
-    opacity: 40%;
-    right: 0;
-    content: "";
-    height: 2035.78px;
-    max-width: 1920px;
-    mix-blend-mode: soft-light;
-    position: absolute;
-    width: 50%;
-    z-index: 1;
-`
+  background: url(/Pattern02.5888be2b14b5c0d295ea.svg) no-repeat;
+  bottom: 15%;
+  opacity: 40%;
+  right: 0;
+  content: '';
+  height: 2035.78px;
+  max-width: 1920px;
+  mix-blend-mode: soft-light;
+  position: absolute;
+  width: 50%;
+  z-index: 1;
+`;
 
-export default After
+export default After;
